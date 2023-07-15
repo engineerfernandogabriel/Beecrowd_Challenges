227639b370b7cc590a1f3e64d9449ec2aef2d1d7
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2023-07-15 00:27:06 -0300

    Beecrowd - Desafio 1000

--- javaScript/Desafio.1000_HelloWorld.js
+++ javaScript/Desafio.1000_HelloWorld.js
@@ -1,1 +1,12 @@
+/*
+ O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+ Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+ENTRADA
+ Este problema não possui nenhuma entrada.
+
+SAíDA
+ Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+*/
+
 console.log("Hello World!")
