6588b2f3073defd766c3c26b33e1461874390f5d
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2023-11-01 23:39:35 -0300

    Desafio 1004 - py

--- python/Desafio_1004_ProdutoSimples.py
+++ python/Desafio_1004_ProdutoSimples.py
@@ -1,4 +1,4 @@
-/*
+'''
  Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
  A seguir mostre a variável PROD com mensagem correspondente.   
 
@@ -8,22 +8,11 @@ ENTRADA
 SAíDA
  Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. 
  Não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
- */
+'''
 
-import java.io.IOException;
-import java.util.Scanner;
+valueA = int(input())
+valueB = int(input())
 
-public class Desafio_1004_ProdutoSimples {
-    public static void main(String[] args) throws IOException{
-        Scanner scan = new Scanner(System.in);
+produto = valueA * valueB
 
-        int valueA = scan.nextInt();
-        int valueB = scan.nextInt();
-
-        int produto = valueA * valueB;
-        
-        scan.close();
-        
-        System.out.printf("PROD = %d\n", produto);
-    }
- }
+print('PROD = %i' %produto)
