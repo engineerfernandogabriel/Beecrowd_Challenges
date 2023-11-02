3bd3bd98cf351a861ded95d5e33137982c0f0b2c
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2023-11-01 23:22:57 -0300

    Desafio 1004 - java

---
+++ java/Desafio_1004_ProdutoSimples.java
@@ -0,0 +1,28 @@
+/*
+ Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+ A seguir mostre a variável PROD com mensagem correspondente.   
+
+ENTRADA
+ O arquivo de entrada contém 2 valores inteiros.
+
+SAÍDA
+ Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. 
+ Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+ */
+
+import java.util.Scanner;
+
+public class Desafio_1004_ProdutoSimples {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int valueA = scan.nextInt();
+        int valueB = scan.nextInt();
+
+        int produto = valueA * valueB;
+        
+        scan.close();
+        
+        System.out.printf("PROD = %d\n", produto);
+    }
+ }
