a673b2a8d198413d3ef07234e49ef3b9407e22f6
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2023-11-15 21:32:10 -0300

    Desafio 1016 - java

---
+++ java/Desafio_1016_Distancia.java
@@ -0,0 +1,32 @@
+/*
+ Dois carros (X e Y) partem em uma mesma direção. 
+ O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+
+ Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue se afastar um quilômetro a 
+cada 2 minutos.
+
+ Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
+
+ENTRADA
+ O arquivo de entrada contém um número inteiro.
+
+SAíDA
+ Imprima o tempo necessário seguido da mensagem "minutos".
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio_1016_Distancia {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        int distancia = scan.nextInt();
+
+        scan.close();
+
+        int tempo = distancia * 2;
+
+        System.out.printf("%d minutos\n", tempo);
+    }
+ }
