2f375e437d9f66c5acbb15ea86cbe0158891aa97
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2023-11-20 23:57:38 -0300

    Desafio 1020 - java

---
+++ java/Desafio_1020_IdadeEmDias.java
@@ -0,0 +1,30 @@
+/*
+Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. 
+Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. 
+Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+
+ENTRADA
+ O arquivo de entrada contém um valor inteiro.
+
+SAíDA
+ Imprima a saída conforme exemplo fornecido.
+*/
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio_1020_IdadeEmDias {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        int idadeDias = scan.nextInt();
+
+        scan.close();
+
+        System.out.printf("%d ano()\n", ano);
+        System.out.printf("%d mes(es)\n", mes);
+        System.out.printf("%d dia(s)\n", dia);
+    }
+}
