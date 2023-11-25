7716a5b3c962b57574d19cfe3c4a73813b747cd2
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2023-11-25 00:05:14 -0300

    Desafio 1036 - python

--- python/Desafio_1036_FormulaDeBaskhara.py
+++ python/Desafio_1036_FormulaDeBaskhara.py
@@ -11,3 +11,19 @@ SAíDA
  Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo 
 abaixo. Imprima sempre o final de linha após cada mensagem.
 '''
+
+import math
+
+valores = list(map(float, input().split()))
+
+valueA, valueB, valueC = valores
+
+delta = (valueB**2) - (4 * valueA * valueC)
+
+if (valueA <= 0 or delta < 0) :
+    print('Impossivel calcular')
+else :
+    raiz1 = (- valueB + math.sqrt(delta)) / (2 * valueA) 
+    raiz2 = (- valueB + math.sqrt(delta)) / (2 * valueA)
+    print('R1 = %.5f' %raiz1)
+    print('R2 = %.5f' %raiz2)
