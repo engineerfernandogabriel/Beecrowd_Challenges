b250c4ac45fa4d9836000a85a0677d0a0c995b68
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2023-12-05 00:44:11 -0300

    Desafio 1042 - python

---
+++ python/Desafio_1042_SortSimples.py
@@ -0,0 +1,54 @@
+'''
+ Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, uma linha em 
+branco e em seguida, os valores na sequência como foram lidos.
+
+ENTRADA
+ A entrada contem três números inteiros.
+
+SAíDA
+ Imprima a saída conforme foi especificado.
+'''
+
+values = list(map(int, input().split()))
+
+valueA, valueB, valueC = values
+
+maior = 0
+meio = 0
+menor = 0
+
+if (valueA < valueB and valueA < valueC) :
+    menor = valueA
+
+    if (valueB < valueC):
+        meio = valueB
+        maior = valueC
+    else :
+        meio = valueC
+        maior = valueB
+elif (valueB < valueC) :
+    menor = valueB
+
+    if (valueA < valueC) :
+        meio = valueA
+        maior = valueC
+    else :
+        meio = valueC
+        maior = valueA
+else :
+    menor = valueC
+
+    if (valueA < valueB) : 
+        meio = valueA
+        maior = valueB
+    else :
+        meio = valueB
+        maior = valueA
+
+print(menor)
+print(meio)
+print(maior)
+print('')
+print(valueA)
+print(valueB)
+print(valueC)
