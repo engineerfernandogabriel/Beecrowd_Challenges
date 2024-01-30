f4aa420ec392d7a9e5e4e3a6b3525c2084db1eda
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-01-30 19:11:26 -0300

    Desafio 1065 - python

---
+++ python/Desafio_1065_ParesEntreCincoNumeros.py
@@ -0,0 +1,19 @@
+'''
+ Faça um programa que leia 5 valores inteiros. 
+ Conte quantos destes valores digitados são pares e mostre esta informação.
+
+ENTRADA
+ O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+SAíDA
+ Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+'''
+
+count = 0
+
+for _ in range(5):
+    value = int(input())
+    if (value % 2 ==0):
+        count += 1
+
+print('%d valores pares' %count)
