f50fbe8b7a62b94b20f1f1d2345e75298e846a83
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-02-03 00:27:40 -0300

    Desafio 1067 - python

---
+++ python/Desafio_1067_NumerosImpares.py
@@ -0,0 +1,14 @@
+'''
+ Leia um valor inteiro X (1 <= X <= 1000). 
+ Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+ENTRADA
+ O arquivo de entrada contém 1 valor inteiro qualquer.
+
+SAíDA
+ Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+'''
+
+value = int(input())
+
+for _ in range(value):
