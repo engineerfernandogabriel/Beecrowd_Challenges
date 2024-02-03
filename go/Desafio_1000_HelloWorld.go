cdb8bb07f2af6ac279e785a5ad532b5569edbe92
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-02-03 00:35:42 -0300

    Desafio 1000 - go

---
+++ go/Desafio_1000_HelloWorld.go
@@ -0,0 +1,18 @@
+/*
+ O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+ Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+ENTRADA
+ Este problema não possui nenhuma entrada.
+
+SAíDA
+ Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World!")
+}
