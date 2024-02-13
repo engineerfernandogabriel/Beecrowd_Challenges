e82e2464c36c4f86f04218f8b16533adcc626d39
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-02-12 23:50:56 -0300

    Desafio 1078 - python

---
+++ python/Desafio_1078_Tabuada.py
@@ -0,0 +1,16 @@
+'''
+ Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
+    1 x N = N      2 x N = 2N        ...       10 x N = 10N
+
+ENTRADA
+A entrada contém um valor inteiro N (2 < N < 1000).
+
+SAíDA
+ Imprima a tabuada de N, conforme o exemplo fornecido.
+'''
+
+n = int(input())
+
+for i in range(1, 11):
+    value = n * i
+    print('%d x %d = %d' %(i, n, value))
