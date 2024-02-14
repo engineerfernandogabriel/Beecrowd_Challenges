ce566ad174ae10f01e8ec49ed5ab798825b569e0
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-02-14 10:33:40 -0300

    Desfaio 1080 - java

---
+++ java/Desafio_1080_MaiorEPosicao.java
@@ -0,0 +1,36 @@
+/*
+ Leia 100 valores inteiros. 
+ Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+
+ENTRADA
+ O arquivo de entrada contém 100 números inteiros, positivos e distintos.
+
+SAíDA
+ Apresente o maior valor lido e a posição de entrada, conforme exemplo abaixo.
+*/
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public abstract class Desafio_1080_MaiorEPosicao {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        int max = 0;
+        int position = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            int value = scan.nextInt();
+
+            if (value > max) {
+                max = value;
+                position = i;
+            }
+        }
+
+        scan.close();
+
+        System.out.println(max);
+        System.out.println(position);
+    }    
+}
