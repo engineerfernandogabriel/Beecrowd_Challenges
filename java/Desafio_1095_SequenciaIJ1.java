9bb96ce9369cb4e2cbd091dea8e9415eaa06a650
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-02-16 20:06:37 -0300

    Desafio 1095 - java

---
+++ java/Desafio_1095_SequenciaIJ1.java
@@ -0,0 +1,29 @@
+/*
+ Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+    I=1 J=60
+    I=4 J=55
+    I=7 J=50
+    ...
+    I=? J=0
+
+ENTRADA
+ Não há nenhuma entrada neste problema.
+
+SAíDA
+ Imprima a sequencia conforme exemplo abaixo
+ */
+
+import java.io.IOException;
+
+public class Desafio_1095_SequenciaIJ1 {
+    public static void main(String[] args) throws IOException {
+        int i = 1;
+
+        for(int j = 60; j >= 0 ; j -= 5) {
+            System.out.printf("I=%d J=%d\n", i, j);
+
+            i += 3;
+        }
+    }
+ }
