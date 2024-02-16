8e206d81b5d711b242d7f486b9dadee38a66d1d2
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-02-16 20:21:26 -0300

    Desafio 1095 - python

---
+++ python/Desafio_1095_SequenciaIJ1.py
@@ -0,0 +1,24 @@
+'''
+ Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+    I=1 J=60
+    I=4 J=55
+    I=7 J=50
+    ...
+    I=? J=0
+
+ENTRADA
+ Não há nenhuma entrada neste problema.
+
+SAíDA
+ Imprima a sequencia conforme exemplo abaixo
+'''
+
+i = 1
+j = 60
+
+while(j >= 0):
+    print('I=%d J=%d' %(i, j))
+
+    i += 3
+    j -= 5   
