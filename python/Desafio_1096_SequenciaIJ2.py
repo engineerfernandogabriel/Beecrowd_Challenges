bbe8a07c0e0e9ebcbd0e2ce6ce87fa07ea2c1e75
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-02-17 16:26:27 -0300

    Desafio 1096 - python

---
+++ python/Desafio_1096_SequenciaIJ2.py
@@ -0,0 +1,24 @@
+'''
+  Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+    I=1 J=7
+    I=1 J=6
+    I=1 J=5
+    I=3 J=7
+    I=3 J=6
+    I=3 J=5
+    ...
+    I=9 J=7
+    I=9 J=6
+    I=9 J=5
+
+ENTRADA
+ Não há nenhuma entrada neste problema.
+
+SAíDA
+ Imprima a sequencia conforme exemplo abaixo
+'''
+
+for i in range (1, 10, 2):
+    for j in range (7, 4, -1):
+        print('I=%d J=%d' %(i,j))
