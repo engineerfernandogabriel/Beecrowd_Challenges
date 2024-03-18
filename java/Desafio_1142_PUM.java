fd3cb0ec7b4c403ba126136c87b4e4b357513762
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-03-18 20:10:58 -0300

    Desafio 1142 - java

--- java/Desafio_1142_PUM.java
+++ java/Desafio_1142_PUM.java
@@ -1,5 +1,6 @@
 /*
- Escreva um programa que leia um valor inteiro N. Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+ Escreva um programa que leia um valor inteiro N. 
+ Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
 
 ENTRADA
  O arquivo de entrada contém um número inteiro positivo N.
