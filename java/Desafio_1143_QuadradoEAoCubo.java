f2d25e38d20edf8c8baa825354844271e31cd739
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-03-19 10:42:58 -0300

    Desafio 1143 - java

---
+++ java/Desafio_1143_QuadradoEAoCubo.java
@@ -0,0 +1,31 @@
+/*
+ Escreva um programa que leia um valor inteiro N (1 < N < 1000). 
+ Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+
+ENTRADA
+ O arquivo de entrada contém um número inteiro positivo N.
+
+SAíDa
+ Imprima a saída conforme o exemplo fornecido.
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio_1143_QuadradoEAoCubo {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            int value2 = i * i;
+            int value3 = i * value2;
+
+            System.out.printf("%d %d %d\n", i, value2, value3);
+        }
+
+        scan.close();
+
+    }
+ }
