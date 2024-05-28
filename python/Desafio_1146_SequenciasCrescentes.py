43f637d1a568b76376c7b6a079043ce18020f89c
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2024-05-28 11:49:37 -0300

    Desafio 1146 - python (Update)

--- python/Desafio_1146_SequenciasCrescentes.py
+++ python/Desafio_1146_SequenciasCrescentes.py
@@ -1,5 +1,5 @@
 '''
- Este programa deve ler uma variável inteira X inúmeras vezes (deve parar quando o valor no arquivo de entrada for igual a zero). 
+ Este programa deve ler uma variável inteira X inúmeras vezes (deve parar quando o valor no arquivo de entrada for igual a zero).
  Para cada valor lido imprima a sequência de 1 até X, com um espaço entre cada número e seu sucessor.
 
  Obs: cuide para não deixar espaço em branco após o último valor apresentado na linha ou você receberá Presentation Error.
@@ -8,6 +8,22 @@ ENTRADA
  O arquivo de entrada contém vários números inteiros. O último número no arquivo de entrada é 0.
 
 SAíDA
- Para cada número N do arquivo de entrada deve ser impressa uma linha de 1 até N, conforme o exemplo abaixo. 
+ Para cada número N do arquivo de entrada deve ser impressa uma linha de 1 até N, conforme o exemplo abaixo.
  Não deve haver espaço em branco após o último valor da linha.
 '''
+
+while (True):
+    n = int(input())
+
+    if (n == 0):
+        break
+    
+    result = ""
+    
+    for i in range (1, n + 1):
+        result += str(i)
+        
+        if(i < n):
+            result += " "
+        
+    print(result)
