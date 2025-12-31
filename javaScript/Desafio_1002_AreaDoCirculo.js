c9b9f773e419c8cd18f309b487c6adcbadb59713
Fernando Gabriel <fernandoramos.sjc@gmail.com>
2025-12-31 15:40:23 -0300

    Javascript - Correção dos descritivos dos exercícios.

--- javaScript/Desafio_1002_AreaDoCirculo.js
+++ javaScript/Desafio_1002_AreaDoCirculo.js
@@ -10,7 +10,7 @@ ENTRADA
 SAíDA
  Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal.
  Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o
- resultado, caso contrário, você receberá "Presentation Error".
+resultado, caso contrário, você receberá "Presentation Error".
 */
 
 const prompt = require("prompt-sync")({sigint:true});
